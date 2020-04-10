d6c0b5eb78300f0a27cec2e427a9ce2244f13db2
evan <aspaasevan@gmail.com>
2020-04-10 08:25:28 -0700

    self-study questions answered

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 import "./App.css";
 
 class App extends React.Component {
@@ -7,6 +8,10 @@ class App extends React.Component {
     this.state = {
       data: []
     };
+  }
+
+  componentDidMount() {
+    
   }
 
   render() {
